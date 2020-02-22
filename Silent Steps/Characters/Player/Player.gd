18e13f6117e5e4f551377a06bd873e92b830c860
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2020-02-22 16:09:07 -0600

    FInished basic player character movement
    Player moves on all axis, and has smooth movement. It looks at mouse position

--- Silent Steps/Characters/Player/Player.gd
+++ Silent Steps/Characters/Player/Player.gd
@@ -1,16 +1,31 @@
 extends "res://Characters/TemplateCharacter.gd"
 
+var motion = Vector2()
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+func _physics_process(delta: float) -> void:
+	move()
+	look_at_mouse()
 
+func handle_movement_input() -> void:
+	# vertical movement
+	if Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+		motion.y = clamp(motion.y - SPEED, -MAX_SPEED, 0)
+	elif Input.is_action_pressed("move_down") and not Input.is_action_pressed("move_up"):
+		motion.y = clamp(motion.y + SPEED, 0, MAX_SPEED)
+	else:
+		motion.y = lerp(motion.y, 0, FRICTION)
+	
+	# horizontal movement
+	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		motion.x = clamp(motion.x - SPEED, -MAX_SPEED, 0)
+	elif Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+		motion.x = clamp(motion.x + SPEED, 0, MAX_SPEED)
+	else:
+		motion.x = lerp(motion.x, 0, FRICTION)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func look_at_mouse():
+	look_at(get_global_mouse_position())
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func move():
+	handle_movement_input()
+	motion = move_and_slide(motion)
